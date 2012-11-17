76b74adfd43943f219e34265f38380a9ae38ec44
Nicholas Ver Voort <moldorma@gmail.com>
2012-11-17 10:33:13 -0500

    Moved headers to Angel.h, having problems compiling on Mac still though

--- main.cpp
+++ main.cpp
@@ -1,16 +1,3 @@
-#ifdef __APPLE__
-#include <OpenGL/gl.h>
-#include <OpenGL/glu.h>
-#include <GLUT/glut.h>
-#else
-#ifdef _WIN32
-#include <windows.h>
-#endif
-#include <GL/gl.h>
-#include <GL/glu.h>
-#include <GL/glut.h>
-#endif
-
 #include "Angel.h"
 #include "vec.h"
 #include "mat.h"
